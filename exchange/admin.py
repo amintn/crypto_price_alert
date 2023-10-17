98c782887969b90449ea566babb11946770d031c
Amin <amin.crasher@gmail.com>
2023-10-17 16:15:43 +0330

    Add SymbolPair model in exchange app

--- exchange/admin.py
+++ exchange/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from exchange.models import Exchange
+from exchange.models import Exchange, SymbolPair
 
 admin.site.register(Exchange)
+admin.site.register(SymbolPair)
--- exchange/models.py
+++ exchange/models.py
@@ -22,3 +22,27 @@ class Exchange(models.Model):
         self.name = self.name.lower().capitalize()
         self.full_clean()
         return super().save(*args, **kwargs)
+
+
+class SymbolPair(models.Model):
+    first_symbol = models.CharField(
+        max_length=8,
+        help_text="First symbol of a pair that replaces the first '{}' of pair's exchange.get_price_url. "
+        "For example 'BTC' for 'BTC-USDT' pair",
+    )
+    second_symbol = models.CharField(
+        max_length=8,
+        help_text="Second symbol of a pair that replaces the second '{}' of pair's exchange.get_price_url. "
+        "For example 'USDT' for 'BTC-USDT' pair",
+    )
+    exchange = models.ForeignKey(Exchange, on_delete=models.CASCADE)
+    current_price = models.DecimalField(max_digits=14, decimal_places=8)
+
+    def __str__(self):
+        return f"{self.first_symbol}-{self.second_symbol}-{self.exchange.name}"
+
+    def save(self, *args, **kwargs):
+        self.first_symbol = self.first_symbol.upper()
+        self.second_symbol = self.second_symbol.upper()
+        self.full_clean()
+        return super().save(*args, **kwargs)
