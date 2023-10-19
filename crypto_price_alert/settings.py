9fdff7429df790377d070dbb96fd8274cda27b8c
Amin <amin.crasher@gmail.com>
2023-10-19 11:11:04 +0330

    Add django-environ package to read from .env

--- crypto_price_alert/settings.py
+++ crypto_price_alert/settings.py
@@ -11,16 +11,21 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from environ import Env
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+env = Env()
+env.read_env(BASE_DIR / ".env")
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-!+f#rx5aem%i11tylvl&olc2bdw(2if15((s_b^n#mc+gdag7="
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
