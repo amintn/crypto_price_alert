0e00c0d9082fe1cdd40c935682d189a0b20615c0
Amin <amin.crasher@gmail.com>
2023-10-20 12:39:36 +0330

    Add Rule model in alert_user app

--- alert_user/admin.py
+++ alert_user/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from alert_user.models import AlertUser
+from alert_user.models import AlertUser, Rule
 
 admin.site.register(AlertUser)
+admin.site.register(Rule)
