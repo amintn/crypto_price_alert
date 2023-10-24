f3c7f9190daa08a5b391ba90e02c9f17c166b85d
Amin <amin.crasher@gmail.com>
2023-10-24 23:16:44 +0330

    Add extract_price method to Exchange instances
    Connect post_init signal from Exchange model to add_extract_method function.
    Make instances of Exchange model have an extract_price method for
    extracting price from json response of get request to url_to_get_price.

--- exchange/apps.py
+++ exchange/apps.py
@@ -1,6 +1,21 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_init
+from django.dispatch import receiver
 
 
 class ExchangeConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "exchange"
+
+    def ready(self):
+        @receiver(post_init, sender="exchange.Exchange")
+        def add_extract_method(instance, **kwargs):
+            keys = instance.extract_price_keys.split("-")
+
+            def extract_price(response_dictionary):
+                unravel = response_dictionary
+                for key in keys:
+                    unravel = unravel.get(key)
+                return unravel
+
+            instance.extract_price = extract_price
---
+++ exchange/signals.py
@@ -0,0 +1,17 @@
+from django.db.models.signals import post_init
+from django.dispatch import receiver
+
+from exchange.models import Exchange
+
+
+@receiver(post_init, sender=Exchange)
+def add_extract_method(instance, **kwargs):
+    keys = instance.json_keys_to_extract_price_from_request.split("-")
+
+    def extract_price(response_dictionary):
+        unravel = response_dictionary
+        for key in keys:
+            unravel = unravel.get(key)
+        return unravel
+
+    instance.extract_price = extract_price
